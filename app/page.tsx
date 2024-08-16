04c51e9aa141ee813016e4feadcb9e813eea1748
hinanaka <h_nakamura@sagin-dp.jp>
2024-08-16 15:54:24 +0900

    page.tsx
    importを””にした

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
