56d1e4d8d3df3d5c5d583e760814a7dc1ebfa8d3
hinanaka <h_nakamura@sagin-dp.jp>
2024-08-19 16:18:20 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -43,10 +43,6 @@ export default function App() {
       {({ signOut, user }) => (
       
     <main>
-
-      <h1>category</h1>
-      <button onClick={createCategory}>+ new</button>
-      
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
